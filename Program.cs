fae618a8298923dd2a0818184b4e5dc995af2b55
Niti Parmar <nitipradhan17@gmail.com>
2026-07-26 23:36:52 +0530

    **Added gitignore file** **Removed unnecessary package referrences**

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using TodoApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
